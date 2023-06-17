d00a8b67a0ca81709878bb363b03245cb0c81815
cy <13990779978@163.com>
2023-06-17 20:54:28 +0800

    refactory lru_k_replacer

--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -19,12 +19,10 @@ LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_fra
 
 auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
   std::lock_guard<std::mutex> lock(latch_);
-
-  // locate the frame
-  frame_id_t evict_id = -1;
+  bool has_eviction = false;
   auto max = std::numeric_limits<size_t>::min();
   for (auto iterator = node_store_.begin(); iterator != node_store_.end(); ++iterator) {
-    LRUKNode &node = iterator->second;
+    auto &node = iterator->second;
     if (!node.IsEvictable()) {
       continue;
     }
@@ -32,23 +30,23 @@ auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
     auto distance = node.GetDistance(current_timestamp_);
     if (distance > max) {
       max = distance;
-      evict_id = iterator->first;
+      has_eviction = true;
+      *frame_id = iterator->first;
     } else if (distance == max) {
-      if (node.GetEariestAccessTime() < node_store_.at(evict_id).GetEariestAccessTime()) {
-        evict_id = iterator->first;
+      has_eviction = true;
+      auto &max_node = node_store_.at(*frame_id);
+      if (node.GetEariestTime() < max_node.GetEariestTime()) {
+        *frame_id = iterator->first;
       }
     }
   }
 
-  // evict the frame
-  if (evict_id != -1) {
+  if (has_eviction) {
     curr_size_--;
-    *frame_id = evict_id;
-    node_store_.erase(evict_id);
-    return true;
+    node_store_.erase(*frame_id);
   }
 
-  return false;
+  return has_eviction;
 }
 
 void LRUKReplacer::RecordAccess(frame_id_t frame_id, [[maybe_unused]] AccessType access_type) {
@@ -60,10 +58,10 @@ void LRUKReplacer::RecordAccess(frame_id_t frame_id, [[maybe_unused]] AccessType
   auto iterator = node_store_.find(frame_id);
   if (iterator != node_store_.end()) {
     auto &node = iterator->second;
-    node.UpdateHistory(++current_timestamp_);
+    node.RecordAccess(current_timestamp_++);
   } else {
     LRUKNode node(k_, frame_id);
-    node.UpdateHistory(++current_timestamp_);
+    node.RecordAccess(current_timestamp_++);
     node_store_.emplace(frame_id, node);
   }
 }
@@ -81,10 +79,8 @@ void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
 
   auto &node = iterator->second;
   if (node.IsEvictable() && !set_evictable) {
-    // node change from evictable to non-evictable
     curr_size_--;
   } else if (set_evictable && !node.IsEvictable()) {
-    // node change from non-evictable to evictable
     curr_size_++;
   }
   node.SetEvictable(set_evictable);
